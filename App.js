b3d0812666fc8271acf1b37f0f98060fe34ee54a
Gabriel Felipe Uribe Lizama <gabriel.uribe@MacBook-Pro-de-Gabriel.local>
2023-01-08 23:46:43 -0300

    first commit in project

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Primera aplicación</Text>
+      <Text>Primera aplicación en Expo</Text>
       <StatusBar style="auto" />
     </View>
   );
